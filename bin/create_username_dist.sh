b676a1178445801a88debda667c2393673e5b579
carri320 <carri320@morris.umn.edu>
2020-09-08 11:17:26 -0500

    Fixed the shellcheck error in create_username.sh (just needed ./ in front of the *)

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -6,7 +6,7 @@ user_dir=$1
 
 cd "$user_dir" || exit
 
-cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* ([a-zA-Z0-9_-]*) [0-9.]*/\1/p' | sort | uniq -c | sed -E -n "s/[ ]*([0-9]*) ([a-zA-Z0-9_-]*)/data.addRow(['\2', \1]);/p" > username_dist.txt
+cat ./*/failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* ([a-zA-Z0-9_-]*) [0-9.]*/\1/p' | sort | uniq -c | sed -E -n "s/[ ]*([0-9]*) ([a-zA-Z0-9_-]*)/data.addRow(['\2', \1]);/p" > username_dist.txt
 
 cd "$work_dir" || exit
 
